7c6954bc71a607df1cb335a73efdad263a1254e9
kumar <sivakumarlella26@email.com>
2024-08-30 10:52:20 +0530

    added special-vars file

---
+++ 08-special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All Varibles: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current Working Directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
