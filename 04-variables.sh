69bb83b35868b1f26e6a481810008a5d54140342
kumar <sivakumarlella26@email.com>
2024-08-30 10:00:35 +0530

    added variables and arguments file

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declaring the variables and arguments
+PERSON1=$1  # no space between=and value
+PERSON2=$2
+
+#refer the variables
+echo "$PERSON1:: Hi $PERSON2, nice to see you."
+echo "$PERSON2:: Hi $PERSON1, glad to meet you."
+echo "$PERSON1:: How many medals you win?"
+echo "$PERSON2:: i won four medals still now"
