324fc36cc448ef2a3bddaa2c752893cc8bc03bd5
kumar <sivakumarlella26@email.com>
2024-08-30 09:52:41 +0530

    added hello-world file

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declaring the variables
+PERSON1=phelps  # no space between=and value
+PERSON2=bolt
+
+#refer the variables
+echo "$PERSON1:: Hi $PERSON2, nice to see you."
+echo "$PERSON2:: Hi $PERSON1, glad to meet you."
+echo "$PERSON1:: How many medals you win?"
+echo "$PERSON2:: i won four medals still now"
