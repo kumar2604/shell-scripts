09182144fc87d7bf5db939c994f3579b693aca22
kumar <sivakumarlella26@email.com>
2024-08-30 09:44:35 +0530

    added hello-world file

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Devilliers:: Hi Kohli, How are you?"
+echo "Kohli:: Hi Devilliers, I am fine , How are you?"
+echo "Devilliers:: I am fine too, How is your health?"
+echo "Kohli:: not bad, I am upgrading my game.
