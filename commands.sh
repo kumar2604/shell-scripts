1c56218e5ba2c6443035366d2b555e51ddaa93dd
kumar <sivakumarlella26@email.com>
2024-08-30 05:46:07 +0530

    added commands file

---
+++ commands.sh
@@ -0,0 +1,10 @@
+# git stages working area , staging/index area, local repo
+
+# you need to push files to working area
+'git add'
+
+# you need to push files to index area
+'git commit' -m "some message"
+
+# you need to push files to local repo
+'git push'
