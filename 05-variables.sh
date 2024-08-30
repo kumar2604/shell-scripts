c6868f1d70ab6107c37fb9c2430d03ac518cff80
kumar <sivakumarlella26@email.com>
2024-08-30 10:34:00 +0530

    added variables file

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "please enter your password::"
+
+read -s PASSWORD  # here PASSWORD is variable
+
+echo "username is: $USERNAME, password is: $PASSWORD"
