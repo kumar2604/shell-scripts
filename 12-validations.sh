d3444babf8b825f22170c2d504d7bbbf364ceb2d
kumar <sivakumarlella26@email.com>
2024-08-31 22:49:00 +0530

    added validations-b file

--- 12-validations.sh
+++ 12-validations.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGFILE=/tmp/$$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [$1 -ne 0 ]
@@ -22,8 +22,8 @@ else
      echo "you are super user."
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-dnf install docker -y
+dnf install docker -y &>>$LOGFILE
 VALIDATE $? "Installing DOCKER"
