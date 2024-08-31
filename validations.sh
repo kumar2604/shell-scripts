6bbb1b692aa036d756629d7339f452ef3bf2cce0
kumar <sivakumarlella26@email.com>
2024-08-31 22:38:07 +0530

    added validations file

---
+++ validations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE(){
+    echo "exit status: $1"
+    echo "what are you doing: $2"
+}
+if [ $USERID -ne o ]
+then 
+     echo "please run this script with root access."
+     exit 1
+else
+     echo "you are super user."
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing MYSQL"
+
+dnf install docker -y
+VALIDATE $? "Installing DOCKER"
