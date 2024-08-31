d69ab63c13886820b0a96ef1356ecd3eebe4c51e
kumar <sivakumarlella26@email.com>
2024-08-31 23:23:08 +0530

    added colours file

--- 13-colours.sh
+++ 13-colours.sh
@@ -6,6 +6,9 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at: $TIMESTAMP"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
