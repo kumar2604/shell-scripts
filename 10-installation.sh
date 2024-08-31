3be02e1fa09709ec306219f1380f5d88d416cd5e
kumar <sivakumarlella26@email.com>
2024-08-31 22:30:02 +0530

    added installation file

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,8 +5,29 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
+    exit 1
 else
     echo "you are super user."
 fi
 
 dnf install nginx -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MYSQL...FAILURE"
+    exit 1
+else
+    echo "Installation of MYSQL...SUCCESS"
+fi
+
+dnf install docker -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of DOCKER...FAILURE"
+    exit 1
+else
+    echo "Installation of DOCKER...SUCCESS"
+fi
+
+
