6138da996d07d46df5858e798176c736fb7ea6b5
kumar <sivakumarlella26@email.com>
2024-09-05 10:32:48 +0530

    current file

---
+++ 14-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Ai from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+#./<script-name>.sh
+
+echo "After calling other script, course: $COURSE"
