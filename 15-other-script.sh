59e043e3bfebfb8d624af636f58ce5242193651d
kumar <sivakumarlella26@email.com>
2024-09-05 10:14:41 +0530

    current file

--- 15-other-script.sh
+++ 15-other-script.sh
@@ -3,4 +3,4 @@
 COURSE="Ai from other script"
 
 echo "variable value from other script, course: $COURSE"
-echo "process id of the current shell script: $$"
+echo "process id of the other shell script: $$"
